8468f58646ac7ff30e81054b435ced767717742a
pryshchenko <pryshchenko.d@gmail.com>
2023-06-20 17:05:00 +0300

    part 7, all work

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const app = express()
-const handlers = require('./lib/handlers')
 
-// Настройка механизма представлений Handlebars.
+const handlers = require('./lib/handlers')
+const weatherMiddlware = require('./lib/middleware/weather')
+
+const app = express()
+
 app.engine('handlebars', expressHandlebars.engine({
- defaultLayout: 'main',
-}))
+    defaultLayout: 'main',
+    helpers: {
+      section: function(name, options) {
+        if(!this._sections) this._sections = {}
+        this._sections[name] = options.fn(this)
+        return null
+      },
+    },
+  }))
+
 app.set('view engine', 'handlebars')
 
 const port = process.env.PORT || 3000
 
+
 app.use(express.static(__dirname + '/public'))
+
+app.use(weatherMiddlware)
 
 app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', handlers.about)
-})
+app.get('/about', handlers.about)
 
-// Пользовательская страница 404
 app.use(handlers.notFound)
-// Пользовательская страница 500
 app.use(handlers.serverError)
-
-
 if (require.main === module) {
     app.listen(port, () => {
     console.log( `Express запущен на http://localhost:${port}` +
     '; нажмите Ctrl+C для завершения.' )
     })
-   } else {
+} else {
     module.exports = app
-   }
+}
